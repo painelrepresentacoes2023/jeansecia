19b5e3d57225eeae5c7213ca21c4e3b08de1975d
painelrepresentacoes2023 <painelrepresentacoes2023@gmail.com>
2025-12-19 09:25:52 -0300

    Update crediario.js

--- js/pages/crediario.js
+++ js/pages/crediario.js
@@ -1,1 +1,9 @@
+export async function renderCrediario() {
+  return `
+    <div class="card">
+      <div class="card-title">Crediário</div>
+      <div class="card-sub">Tela em construção. Próximo passo: parcelas, vencimentos, pagamentos parciais e alertas.</div>
+    </div>
+  `;
+}
 
