e488bbe7be62aa54fafeb0a45241d5810a5930ed
painelrepresentacoes2023 <painelrepresentacoes2023@gmail.com>
2025-12-19 09:24:53 -0300

    Update produtos.js

--- js/pages/produtos.js
+++ js/pages/produtos.js
@@ -1,1 +1,9 @@
+export async function renderProdutos() {
+  return `
+    <div class="card">
+      <div class="card-title">Produtos</div>
+      <div class="card-sub">Tela em construção. Próximo passo: cadastro + categorias + grade.</div>
+    </div>
+  `;
+}
 
