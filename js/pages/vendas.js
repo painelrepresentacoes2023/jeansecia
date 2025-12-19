9f208eb85f02fcc55bad48b0d2ab047be082257f
painelrepresentacoes2023 <painelrepresentacoes2023@gmail.com>
2025-12-19 09:25:38 -0300

    Update vendas.js

--- js/pages/vendas.js
+++ js/pages/vendas.js
@@ -1,1 +1,9 @@
+export async function renderVendas() {
+  return `
+    <div class="card">
+      <div class="card-title">Vendas</div>
+      <div class="card-sub">Tela em construção. Próximo passo: venda rápida + desconto + baixa no estoque.</div>
+    </div>
+  `;
+}
 
