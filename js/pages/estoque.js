6ad13fcc0cfd6767823cafcbcfdbfba957c58275
painelrepresentacoes2023 <painelrepresentacoes2023@gmail.com>
2025-12-19 09:25:08 -0300

    Update estoque.js

--- js/pages/estoque.js
+++ js/pages/estoque.js
@@ -1,1 +1,9 @@
+export async function renderEstoque() {
+  return `
+    <div class="card">
+      <div class="card-title">Estoque</div>
+      <div class="card-sub">Tela em construção. Próximo passo: filtros (categoria, cor, tamanho) + tabela.</div>
+    </div>
+  `;
+}
 
