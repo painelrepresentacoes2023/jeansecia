513f478f5d357500192da2729cc52fd497863403
painelrepresentacoes2023 <painelrepresentacoes2023@gmail.com>
2025-12-19 09:25:23 -0300

    Update compras.js

--- js/pages/compras.js
+++ js/pages/compras.js
@@ -1,1 +1,9 @@
+export async function renderCompras() {
+  return `
+    <div class="card">
+      <div class="card-title">Compras</div>
+      <div class="card-sub">Tela em construção. Próximo passo: registrar compras e entrada automática no estoque.</div>
+    </div>
+  `;
+}
 
